781c9f0a6f06ca5ebf35c0c99386ee6bd08977cd
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-06-04 18:01:56 -0400

    Active styling

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { NavLink as Link } from 'react-router-dom'
+import { NavLink as NLink, Link } from 'react-router-dom'
 
 export const Nav = styled.nav`
     background-color: rgb(27, 28, 28);
@@ -18,16 +18,17 @@ export const HomeShortcut = styled(Link)`
     height: 100%;
 `
 
-export const NavLink = styled(Link)`
+export const NavLink = styled(NLink)`
     color: #fff;
     display: flex;
     align-items: center;
     text-decoration: none;
     padding 0 1rem;
     height: 50%;
+    transition: color .2s ease-in-out;
 
     &:hover {
-        color: rgb(16, 51, 112);
+        color: rgb(6, 49, 104);
         transition: color .2s ease-in-out;
     }
 `
