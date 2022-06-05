7bf748cdd324a48173814876ce968381354a226b
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-06-04 21:24:07 -0400

    Add project page description

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,9 +8,13 @@ const BucketHatDescription = "A relatively simple Discord bot written in java."
 const Projects = () => {
     return (
         <div>
-            <h1 className="text-center">Projects</h1>
+            <div className="text-center">
+                <h1 className="text-center" style={{ paddingBottom: `5px` }}>Projects</h1>
+                <p>You can find all of my projects below. I don't usually work on them besides some tinkering here and there.
+                    They're all open source, and you can find their respective GitHub links in their cards.</p>
+            </div>
             <div className="simple-flex-grid">
-                <Card maxWidth="25vw" title="Bucket Hat" description={BucketHatDescription} className="simple-flex-col" />
+                <Card maxWidth="20vw" title="Bucket Hat" description={BucketHatDescription} className="simple-flex-col" />
             </div>
         </div>
     )
