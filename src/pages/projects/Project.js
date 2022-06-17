bbd34db5381637d46341e58c738f20d0d1a0d2ed
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-06-17 18:03:44 -0400

    Margin and container div

--- src/pages/projects/Project.js
+++ src/pages/projects/Project.js
@@ -4,11 +4,11 @@ import "./ProjectPage.css";
 class Project extends React.Component {
     render() {
         return (
-            <>
+            <div className="projectContainer">
                 <h1>{this.title}</h1>
                 <p><i>{this.simpleDescription}</i></p>
                 {this.description}
-            </>
+            </div>
         );
     }
 }
