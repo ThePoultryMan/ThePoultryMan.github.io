4cb212e507270f0b4b7b79f960e7bbd622b44d6d
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-06-17 18:50:52 -0400

    (i was too caught up in the antics)

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,21 +3,21 @@ import { Nav, HomeShortcut, NavLink, NavMenu } from './NavbarElements'
 import Sidebar from '../Sidebar'
 
 const Navbar = () => {
-return (
-<>
-    <Nav>
-        <HomeShortcut to='./'>
-            <h1>Carbon Toast</h1>
-        </HomeShortcut>
-        <NavMenu>
-            <NavLink to='./'>Home</NavLink>
-            <NavLink to='./projects'>Projects</NavLink>
-            <NavLink to='./mods'>Mods</NavLink>
-        </NavMenu>
-    </Nav>
-<Sidebar />
-</>
-)
+    return (
+        <>
+            <Nav>
+                <HomeShortcut to='./'>
+                    <h1>Carbon Toast</h1>
+                </HomeShortcut>
+                <NavMenu>
+                    <NavLink to='./'>Home</NavLink>
+                    <NavLink to='./projects'>Projects</NavLink>
+                    <NavLink to='./mods'>Mods</NavLink>
+                </NavMenu>
+            </Nav>
+            <Sidebar />
+        </>
+    )
 }
 
 export default Navbar
