4b0097e4ca2bf9030ca15106223d1105a9c0eede
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-06-16 23:01:20 -0400

    Updates

---
+++ precache-manifest.4292a49969b9f4e87ca7090fe1433fab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3edb3bef7674f24fb7f0",
+    "url": "/static/css/main.3e42973a.chunk.css"
+  },
+  {
+    "revision": "3edb3bef7674f24fb7f0",
+    "url": "/static/js/main.e6ab1999.chunk.js"
+  },
+  {
+    "revision": "542aac0cd5d471e5987f",
+    "url": "/static/js/runtime~main.b0b8ec09.js"
+  },
+  {
+    "revision": "8bc70c7a5eb09122716c",
+    "url": "/static/js/2.8211f6a1.chunk.js"
+  },
+  {
+    "revision": "50f4a02157d83cc5f2d4",
+    "url": "/static/js/3.09a0fe09.chunk.js"
+  },
+  {
+    "revision": "73c45b1a4ee0bc8f50fbe8da85db5037",
+    "url": "/index.html"
+  }
+];
